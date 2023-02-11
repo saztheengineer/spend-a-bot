f993bd4d806bbe8ee7244b1a24082040a436a90d
Sarah <sarahplant@live.co.uk>
2023-02-11 16:51:30 +0000

    Updated

--- savings.py
+++ savings.py
@@ -1,13 +1,12 @@
 print("Ready to get your spending in check? Lets start with some common outgoings..")
 
 rent = input ("Rent: £")
+bills = input("Bills (Water, Gas & Electric): £")
 food = input ("Food: £")
 phoneBill = input ("Phone Bill: £")
 tv = input ("TV/Broadband: £")
 gym = input("Gym Membership: £")
-bills = input("Bills (Water, Gas & Electric): £")
 
-
-totalSpending = int(rent) + int(food) + int(phoneBill) + int(tv) + int(gym) + int(bills)
+totalSpending = int(rent) + int(bills) + int(food) + int(phoneBill) + int(tv) + int(gym) 
 
 print(totalSpending)
